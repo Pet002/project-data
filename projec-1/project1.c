a7a3459f330764da8585e75393ae1ec8684a72fc
ธนโชติ ศักดิ์ธรรมเจริญ <pet.14336@gmail.com>
2022-02-09 19:04:01 +0700

    Project-1-Finish

--- projec-1/project1.c
+++ projec-1/project1.c
@@ -1,73 +1,169 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 
-typedef struct nd{
+typedef struct nd
+{
     char c;
     struct nd *next;
-}node;
+} node1;
 
-node *top = NULL;
+typedef struct new
+{
+    double num;
+    struct new *next;
+}
+node2;
 
+node2 *top2 = NULL;
 
-void push(char x){
-    node *n = malloc(sizeof(node));
+node1 *top = NULL;
+
+// infix to postfix algorithem
+void push(char x)
+{
+    node1 *n = malloc(sizeof(node1));
     n->next = top;
     top = n;
     n->c = x;
 }
 
-char pop(){
+char pop()
+{
     char p;
-    node *n;
+    node1 *n;
     n = top;
-    top = top -> next;
+    top = top->next;
     p = n->c;
     free(n);
     return p;
 }
 
-char stacktop(){
-    if(top == NULL)
+char stacktop()
+{
+    if (top == NULL)
         return '\0';
     else
-        return top -> c;
+        return top->c;
 }
 
-int checkpr(char temp){
+int checkpr(char temp)
+{
     int pr;
-    if(temp == '(')
+    if (temp == '(')
         return 5;
-    else if(temp == '^'){
+    else if (temp == '^')
+    {
         return 4;
     }
-    else if(temp == '*' || temp == '/'){
+    else if (temp == '*' || temp == '/')
+    {
         return 2;
-    }else{
+    }
+    else
+    {
         return 1;
     }
 }
 
-
-void checkoper(char ck){
-    if(stacktop() == '\0' || stacktop() == '('){
+void checkoper(char ck)
+{
+    if (stacktop() == '\0' || stacktop() == '(')
+    {
         push(ck);
     }
-    else{
+    else
+    {
         if (checkpr(ck) <= checkpr(stacktop()))
         {
-            while ( (checkpr(ck) <= checkpr(stacktop())) && stacktop() != '\0' )
+            while ((checkpr(ck) <= checkpr(stacktop())) && stacktop() != '\0')
             {
-                printf("%c " ,pop());
+                printf("%c ", pop());
             }
-            
         }
         push(ck);
     }
-    
 }
 
-int main(int argc, char const *argv[])
+void prefixToPostfix()
+{
+    char ch;
+    while ((ch = getchar()) != '\n')
+    {
+        if (isdigit(ch) || isalpha(ch))
+            printf("%c ", ch);
+        else if (ch == ')')
+        {
+            while (stacktop() != '(')
+            {
+                printf("%c ", pop());
+            }
+            pop();
+        }
+        else
+            checkoper(ch);
+    }
+    while (stacktop() != '\0')
+    {
+        printf("%c ", pop());
+    }
+}
+
+// postfix to result
+void push2(double num)
+{
+    node2 *n = malloc(sizeof(node2));
+    n->next = top2;
+    top2 = n;
+    n->num = num;
+}
+
+double pop2()
+{
+    node2 *collect;
+    double numcol;
+
+    collect = top2;
+    top2 = top2->next;
+    numcol = collect->num;
+    free(collect);
+    return numcol;
+}
+
+void postToResult(char num)
+{
+    if (isdigit(num))
+    {
+        double n = num - '0';
+        push2(n);
+    }
+    else
+    {
+        double n1 = pop2();
+        double n2 = pop2();
+        if (num == '+')
+            push2(n2 + n1);
+        else if (num == '-')
+            push2(n2 - n1);
+        else if (num == '*')
+            push2(n2 * n1);
+        else if (num == '/')
+            push2(n2 / n1);
+    }
+}
+
+void postfixToResult()
+{
+    char ch;
+    while ((ch = getchar()) != '\n')
+    {
+        postToResult(ch);
+    }
+    printf("%.2f", top2->num);
+}
+
+// main
+/*int main(int argc, char const *argv[])
 {
     char ch;
     while ((ch = getchar()) != '\n')
@@ -87,6 +183,89 @@ int main(int argc, char const *argv[])
     while(stacktop() != '\0'){
         printf("%c ", pop());
     }
-    
+
+    return 0;
+}*/
+
+// color set
+
+void red()
+{
+    printf("\033[1;31m");
+}
+void yellow()
+{
+    printf("\033[1;33m");
+}
+void reset()
+{
+    printf("\033[0m");
+}
+
+int main(int argc, char const *argv[])
+{
+    red();
+    printf("1 Infix To Postfix Function\n");
+    printf("2 Postfix To Result Function\n");
+    printf("Enter Your Selection : ");
+    char check, invalid;
+    scanf("%c" , &check);
+    scanf("%c" , &invalid);
+    if (check == '1')
+    {
+        yellow();
+        printf("--------------------------------------------\n");
+        printf("Your infix is : ");
+        char ch;
+        int value = 1;
+        while (scanf("%c" , &ch) && ch != '\n')
+        {
+            if(value){
+                printf("Your Answer is : ");
+                value = 0;
+            }
+            if (isdigit(ch) || isalpha(ch))
+                printf("%c ", ch);
+            else if (ch == ')')
+            {
+                while (stacktop() != '(')
+                {
+                    printf("%c ", pop());
+                }
+                pop();
+            }
+            else
+                checkoper(ch);
+        }
+        while (stacktop() != '\0')
+        {
+            printf("%c ", pop());
+        }
+        printf("\n--------------------------------------------\n");
+
+    }
+    else if (check == '2')
+    {
+        yellow();
+        printf("Your postfix is : ");
+        char ch;
+        while (scanf("%c" , &ch) && ch != '\n')
+        {
+            postToResult(ch);
+        }
+        
+        printf("--------------------------------------------\n");
+        printf("Your result is : ");
+        printf("%.2f", top2->num);
+        printf("\n--------------------------------------------\n");
+
+    }
+    else
+    {
+        reset();
+        printf("Incorrect...");
+    }
+    reset();
+
     return 0;
 }
